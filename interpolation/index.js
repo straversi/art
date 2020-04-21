4acb1c530873ee087a8460cb654f143a80201139
Steven Traversi <straversi@berkeley.edu>
2020-04-20 23:32:36 -0700

    improve interpolation UI

--- interpolation/index.js
+++ interpolation/index.js
@@ -2,11 +2,6 @@ const width = 400;
 const height = 600;
 
 /**
- * Input value: number of horizontal lines.
- */
-const nLines = 120;
-
-/**
  * Input value: number of control points in each line.
  */
 const nPoints = 100;
@@ -14,6 +9,8 @@ const nPoints = 100;
 
 // Curves that have been added by the user.
 let inputCurves = [];
+// Lines between input Curves and top/bottom lines.
+let linesBetween = [60];
 
 /**
  * p5 will run this function first.
@@ -26,11 +23,12 @@ function setup() {
   noFill();
   noLoop();
 
-  document.addEventListener('equationUpdate', (e) => {
+  document.addEventListener('update', (e) => {
     inputCurves = [];
     for (const equation of e.detail.equations) {
       inputCurves.push(makeInputLine(equation));
     }
+    linesBetween = e.detail.linesBetween;
     redraw();
   });
   
@@ -47,9 +45,13 @@ function makeInputLine(equationString) {
   for (let x = -1; x < 1; x += 2 / nPoints) {
     // Offset x, y by width/2, height/2 to center the equation
     // on the canvas size.
+    const y = eval(equationString);
+    // TODO: catch errors on eval and print to the UI. Requires
+    // knowing when to delete the visible error state, e.g.
+    // i.e. which methods cause errors.
     line.push([
       (x * width/2) + width/2,
-      -1 * (eval(equationString) * width/2) + height/2,
+      -1 * (y * width/2) + height/2,
     ]);
   }
   return line;
@@ -106,18 +108,21 @@ function drawCurve(points) {
  * p5 will run this function after setup. Draw stuff here.
  */
 function draw() {  
-  // Clears the canvas.
-  background(255, 255, 255);
+  // Reach into private clearCanvas. `background(255, 255, 255)`
+  // does not work as expected with p5.svg.js extension; if used,
+  // the extension will not clear out existing objects before
+  // adding a new solid rect. This leads to massive file sizes.
+  drawingContext.__clearCanvas();
 
   const topLine = makeLine([0, 0], [width, 0], nPoints);
   const bottomLine = makeLine([0, height], [width, height], nPoints);
 
   const curvesToDraw = [topLine, ...inputCurves, bottomLine];
-  const linesBetweenCurves = nLines / (curvesToDraw.length - 1);
   // Inerpolate between the curves.
   for (let i = 0; i < curvesToDraw.length - 1; i++) {
     const fromCurve = curvesToDraw[i];
     const toCurve = curvesToDraw[i+1];
+    const linesBetweenCurves = linesBetween[i];
     for (let t = 0; t < 1; t += 1 / linesBetweenCurves) {
       drawCurve(lerpCurves(fromCurve, toCurve, t));
     }
